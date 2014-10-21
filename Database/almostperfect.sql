a0786d5a6a28746b7ee4d30ff86b54a225a7da72
sbakiu <sbakiu@gmail.com>
2014-10-21 12:29:13 +0200

    database
    Added location

--- Database/almostperfect.sql
+++ Database/almostperfect.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 21, 2014 at 09:36 AM
+-- Generation Time: Oct 21, 2014 at 12:27 PM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -23,6 +23,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `genders`
+--
+
+CREATE TABLE IF NOT EXISTS `genders` (
+  `id` int(11) NOT NULL,
+  `description` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `genders`
+--
+
+INSERT INTO `genders` (`id`, `description`) VALUES
+(1, 'Male'),
+(2, 'Female');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -31,21 +50,28 @@ CREATE TABLE IF NOT EXISTS `users` (
   `Password` varchar(50) NOT NULL,
   `Enabled` tinyint(1) NOT NULL,
   `Name` varchar(50) NOT NULL,
-  `Orientation` varchar(11) NOT NULL,
+  `looking` int(11) NOT NULL,
   `Age` int(11) NOT NULL,
-  `email` varchar(50) NOT NULL
+  `email` varchar(50) NOT NULL,
+  `gender` int(11) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `pic` blob NOT NULL,
+  `lat` float NOT NULL,
+  `lon` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`Username`, `Password`, `Enabled`, `Name`, `Orientation`, `Age`, `email`) VALUES
-('fdsa', '44', 0, 'df', '0', 43, 'dsaf'),
-('popoi', 'gfff', 0, 'popo', '0', 90, 'ppp'),
-('sbakiu', 'test', 0, 'Sadik Bakiu', '0', 24, 'sbakiu@gmail.com'),
-('sbakiu', 'test', 0, 'Sadik Bakiu', '0', 24, 'sbakiu@gmail.com'),
-('sbakiu1', 'test', 0, 'Sadik Bakiu', '0', 24, 'sbakiu1@gmail.com');
+INSERT INTO `users` (`Username`, `Password`, `Enabled`, `Name`, `looking`, `Age`, `email`, `gender`, `location`, `pic`, `lat`, `lon`) VALUES
+('fdsa', '44', 0, 'df', 0, 43, 'dsaf', 0, '', '', 0, 0),
+('popoi', 'gfff', 0, 'popo', 0, 90, 'ppp', 0, '', '', 0, 0),
+('sbakiu', 'test', 0, 'Sadik Bakiu', 0, 24, 'sbakiu@gmail.com', 0, '', '', 0, 0),
+('sbakiu', 'test', 0, 'Sadik Bakiu', 0, 24, 'sbakiu@gmail.com', 0, '', '', 0, 0),
+('sbakiu1', 'test', 0, 'Sadik Bakiu', 0, 24, 'sbakiu1@gmail.com', 0, '', '', 0, 0),
+('bbakiu', 'test', 0, 'Bujar Bakiu', 2, 24, 'bakiu.bujar@hotmail.com', 1, 'Aachen', '', 0, 0),
+('sbakiu2', 'test', 0, 'Sadik Bakiu', 2, 24, 'sbakiu2@gmail.com', 1, 'Aachen', '', 0, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
